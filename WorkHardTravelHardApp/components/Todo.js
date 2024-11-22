db32e7fee71f3e69b98b14f1d8f6dd59c795eb9c
Mukho <mukho@kakao.com>
2024-11-22 22:16:19 +0900

    React Native for Web and Platform

--- WorkHardTravelHardApp/components/Todo.js
+++ WorkHardTravelHardApp/components/Todo.js
@@ -1,28 +1,41 @@
 import React, { useContext, useState } from "react";
-import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TouchableOpacity, Alert, Platform } from "react-native";
 import { CheckBox } from "react-native-elements";
 import { Fontisto } from "@expo/vector-icons";
 import { WorkingContext } from "../context/WorkingContext";
 import { saveToDos, getDeletedToDos, updateToDos } from "../asyncStorage";
 import styles from "../styles/TodoStyles";
 
+const OS = Platform.OS;
+
 const Todo = ({ objectKey, toDo }) => {
   const { setToDos } = useContext(WorkingContext);
   const [isChecking, setIsChecking] = useState(toDo.checking);
 
+  const confirmDelete = async (key) => {
+    const deletedToDos = await getDeletedToDos(key);
+    setToDos({ ...deletedToDos });
+    await saveToDos({ ...deletedToDos });
+  };
+
   const deleteToDo = async (key) => {
     try {
-      Alert.alert("삭제", "정말 삭제하실 건가요?", [
-        { text: "아니오" },
-        {
-          text: "예",
-          onPress: async () => {
-            const deletedToDos = await getDeletedToDos(key);
-            setToDos({ ...deletedToDos });
-            await saveToDos({ ...deletedToDos });
+      if (OS === "web") {
+        const ok = confirm("정말 삭제하실 건가요?");
+        if (ok) {
+          await confirmDelete(key);
+        }
+      } else {
+        Alert.alert("삭제", "정말 삭제하실 건가요?", [
+          { text: "아니오" },
+          {
+            text: "예",
+            onPress: async () => {
+              await confirmDelete(key);
+            },
           },
-        },
-      ]);
+        ]);
+      }
     } catch (e) {}
   };
 
